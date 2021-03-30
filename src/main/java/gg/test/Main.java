7921734d10832957ef39d5faf5d4579312f7783b
Balazs Szanka <szankabalazs@gmail.com>
2021-03-30 12:39:25 +0200

    orianna API added

--- src/main/java/gg/test/Main.java
+++ src/main/java/gg/test/Main.java
@@ -1,54 +1,41 @@
 package gg.test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import net.rithms.riot.api.ApiConfig;
-import net.rithms.riot.api.RiotApi;
-import net.rithms.riot.api.RiotApiException;
-import net.rithms.riot.api.endpoints.match.dto.MatchList;
-import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
-import net.rithms.riot.constant.Platform;
+import com.merakianalytics.orianna.Orianna;
+import com.merakianalytics.orianna.types.common.Region;
+import com.merakianalytics.orianna.types.core.championmastery.ChampionMasteries;
+import com.merakianalytics.orianna.types.core.championmastery.ChampionMastery;
+import com.merakianalytics.orianna.types.core.searchable.SearchableList;
+import com.merakianalytics.orianna.types.core.staticdata.Champion;
+import com.merakianalytics.orianna.types.core.summoner.Summoner;
 
 public class Main {
+    public static void main(final String[] args) {
+        // API git: https://github.com/meraki-analytics/orianna
+        // RIOT API kulcs beállítása alternatív módszer: https://orianna.readthedocs.io/en/latest/configuring-orianna.html
+        // Logolás bekapcsolása:
+        //BasicConfigurator.configure();
+        String key = "RGAPI-760c5258-a109-419d-9a9d-5734e13b3189";
+        Orianna.setRiotAPIKey(key);
+        String name = "balazs337";
+        String champ = "Varus";
+        Region region = Region.EUROPE_NORTH_EAST;
+        final Summoner summoner = Summoner.named(name).withRegion(region).get();
+        final Champion champion = Champion.named(champ).withRegion(region).get();
+        final ChampionMastery cm = summoner.getChampionMastery(champion);
+//        System.out.println("Champion ID: " + cm.getChampion().getId());
+        System.out.println("Mastery points: " + cm.getPoints());
+        System.out.println("Mastery level: " + cm.getLevel());
+        System.out.println("Points until next level: " + cm.getPointsUntilNextLevel());
 
-    public static void main(String[] args) throws RiotApiException, IOException {
+        // ChampionMasteries cms = ChampionMasteries.forSummoner(summoner).get();
+        final ChampionMasteries cms = summoner.getChampionMasteries();
+        System.out.println(cms.get(3).getPoints());
+        System.out.println(cms.find(champion.getName()).getPoints());
 
-
-        String key = "RGAPI-4ef814bf-bb74-4f8f-bc13-e33c597b2372";
-        Platform platform = Platform.EUNE;
-
-//        Path file = FileSystems.getDefault().getPath(args[0]);
-//        Charset charset = Charset.forName("US-ASCII");
-//
-//        try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
-//            String line = null;
-//            if ((line = reader.readLine()) != null)
-//                key = line;
-//        }
-
-        ApiConfig config = new ApiConfig().setKey(key);
-        RiotApi api = new RiotApi(config);
-        Summoner summoner = api.getSummonerByName(platform, args[0]);
-        MatchList matchList = api.getMatchListByAccountId(platform, summoner.getAccountId());
-
-        System.out.println("Total Games in requested match list: " + matchList.getTotalGames());
-
-//        // We can now iterate over the match list to access the data
-//        if (matchList.getMatches() != null) {
-//            for (MatchReference match : matchList.getMatches()) {
-//                System.out.println("GameID: " + match.getGameId());
-//            }
-//        }
-        System.out.println("Name: " + summoner.getName());
-        System.out.println("Summoner ID: " + summoner.getId());
-        System.out.println("Account ID: " + summoner.getAccountId());
-        System.out.println("PUUID: " + summoner.getPuuid());
-        System.out.println("Summoner Level: " + summoner.getSummonerLevel());
-        System.out.println("Profile Icon ID: " + summoner.getProfileIconId());
+        System.out.println(summoner.getName() + " has mastery level 6 or higher on:");
+        final SearchableList<ChampionMastery> pro = cms.filter((final ChampionMastery mastery) -> mastery.getLevel() >= 6);
+        for(final ChampionMastery mastery : pro) {
+            System.out.println(mastery.getChampion().getName());
+        }
     }
 }
