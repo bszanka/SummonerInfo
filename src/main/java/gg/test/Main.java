4d02b3fc7428c38d436b2582b49087fa79ea964b
Balazs Szanka <szankabalazs@gmail.com>
2021-03-31 08:09:12 +0200

    Top 10 played champs, Last match data added

--- src/main/java/gg/test/Main.java
+++ src/main/java/gg/test/Main.java
@@ -6,11 +6,20 @@ import com.merakianalytics.orianna.types.common.Region;
 import com.merakianalytics.orianna.types.common.Season;
 import com.merakianalytics.orianna.types.core.championmastery.ChampionMasteries;
 import com.merakianalytics.orianna.types.core.championmastery.ChampionMastery;
+import com.merakianalytics.orianna.types.core.match.Match;
 import com.merakianalytics.orianna.types.core.match.MatchHistory;
+import com.merakianalytics.orianna.types.core.match.Participant;
 import com.merakianalytics.orianna.types.core.searchable.SearchableList;
 import com.merakianalytics.orianna.types.core.staticdata.Champion;
+import com.merakianalytics.orianna.types.core.staticdata.Champions;
 import com.merakianalytics.orianna.types.core.summoner.Summoner;
 import org.apache.log4j.BasicConfigurator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.*;
 
 
 public class Main {
@@ -32,23 +41,23 @@ public class Main {
         String champ = "Varus";
         Region region = Region.EUROPE_NORTH_EAST;
         final Summoner summoner = Summoner.named(name).withRegion(region).get();
-        final Champion champion = Champion.named(champ).withRegion(region).get();
-        final ChampionMastery cm = summoner.getChampionMastery(champion);
-//        System.out.println("Champion ID: " + cm.getChampion().getId());
-        System.out.println("Mastery points: " + cm.getPoints());
-        System.out.println("Mastery level: " + cm.getLevel());
-        System.out.println("Points until next level: " + cm.getPointsUntilNextLevel());
-
-        // ChampionMasteries cms = ChampionMasteries.forSummoner(summoner).get();
-        final ChampionMasteries cms = summoner.getChampionMasteries();
-//        System.out.println(cms.get(3).getPoints());
-//        System.out.println(cms.find(champion.getName()).getPoints());
-
-        System.out.println(summoner.getName() + " has mastery level 6 or higher on:");
-        final SearchableList<ChampionMastery> pro = cms.filter((final ChampionMastery mastery) -> mastery.getLevel() >= 6);
-        for(final ChampionMastery mastery : pro) {
-            System.out.println(mastery.getChampion().getName());
-        }
+//        final Champion champion = Champion.named(champ).withRegion(region).get();
+//        final ChampionMastery cm = summoner.getChampionMastery(champion);
+////        System.out.println("Champion ID: " + cm.getChampion().getId());
+//        System.out.println("Mastery points: " + cm.getPoints());
+//        System.out.println("Mastery level: " + cm.getLevel());
+//        System.out.println("Points until next level: " + cm.getPointsUntilNextLevel());
+//
+//        // ChampionMasteries cms = ChampionMasteries.forSummoner(summoner).get();
+//        final ChampionMasteries cms = summoner.getChampionMasteries();
+////        System.out.println(cms.get(3).getPoints());
+////        System.out.println(cms.find(champion.getName()).getPoints());
+//
+//        System.out.println(summoner.getName() + " has mastery level 6 or higher on:");
+//        final SearchableList<ChampionMastery> pro = cms.filter((final ChampionMastery mastery) -> mastery.getLevel() >= 6);
+//        for(final ChampionMastery mastery : pro) {
+//            System.out.println(mastery.getChampion().getName());
+//        }
 
 
 //        // RATE LIMIT EXCEEDED :(((
@@ -86,6 +95,70 @@ public class Main {
 //                unpulledMatchIds.remove(newMatchId);
 //                pulledMatchIds.add(newMatchId);
 //            }
+//        }
+
+
+
+        final MatchHistory matchHistory = MatchHistory.forSummoner(summoner).get();
+        // MatchHistory match_history = MatchHistory.forSummoner(summoner).withQueues([Queue.RANKED_SOLO_5x5]).withSeasons([Season.SEASON_7]).get();
+
+        // Load the entire match history by iterating over all its elements so that we know how long it is.
+        // Unfortunately since we are iterating over the match history and accessing the summoner's champion for each match,
+        // we need to know what version of the static data the champion should have. To avoid pulling many different
+        // static data versions, we will instead create a {champion_id -> champion_name} mapping and just access the champion's
+        // ID from the match data (which it provides directly).
+        final Map<Integer, String> championIdToNameMapping = new HashMap<>();
+        for(final Champion champion : Champions.withRegion(region).get()) {
+            championIdToNameMapping.put(champion.getId(), champion.getName());
+        }
+        final Map<String, Integer> playedChampions = new HashMap<>();
+        for(final Match match : matchHistory) {
+            final Integer championId = match.getParticipants().find(summoner).getChampion().getId();
+            final String championName = championIdToNameMapping.get(championId);
+            Integer count = playedChampions.get(championName);
+            if(count == null) {
+                count = 0;
+                playedChampions.put(championName, count);
+            }
+            playedChampions.put(championName, playedChampions.get(championName) + 1);
+        }
+        System.out.println("Length of match history: " + matchHistory.size());
+
+        // Top 10 champ
+        final List<Entry<String, Integer>> entries = new ArrayList<>(playedChampions.entrySet());
+        entries.sort((final Entry<String, Integer> e0, final Entry<String, Integer> e1) -> Integer.compare(e1.getValue(), e0.getValue()));
+
+        for(int i = 0; i < 10 && i < entries.size(); i++) {
+            final String championName = entries.get(i).getKey();
+            final int count = entries.get(i).getValue();
+            System.out.println(championName + " " + count);
+        }
+
+//        // Utolsó meccs
+//        System.out.println("\n");
+//
+//        final Match match = matchHistory.get(0);
+//        System.out.println("Match ID: " + match.getId());
+//
+//        final Participant participant = match.getParticipants().find(summoner);
+//        System.out.println("\nSince the match was created from a matchref, we only know one participant:");
+//        System.out.println(participant.getSummoner().getName() + " played " + participant.getChampion().getName());
+//
+//        System.out.println("\nNow pull the full match data by iterating over all the participants:");
+//        for(final Participant p : match.getParticipants()) {
+//            System.out.println(p.getSummoner().getName() + " played " + p.getChampion().getName());
+//        }
+//
+//        System.out.println("\nIterate over all the participants again and note that the data is not repulled:");
+//        for(final Participant p : match.getParticipants()) {
+//            System.out.println(p.getSummoner().getName() + " played " + p.getChampion().getName());
+//        }
+//
+//        System.out.println("\nBlue team won? " + match.getBlueTeam().isWinner());
+//        System.out.println("Red team won? " + match.getRedTeam().isWinner());
+//        System.out.println("Participants on blue team:");
+//        for(final Participant p : match.getBlueTeam().getParticipants()) {
+//            System.out.println(p.getSummoner().getName());
 //        }
     }
 }
