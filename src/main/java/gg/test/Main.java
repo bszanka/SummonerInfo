891be1c13c04861c7afd48c6152c07738fafca4b
Balazs Szanka <szankabalazs@gmail.com>
2021-03-31 11:40:08 +0200

    fxml classloader works now. needs cleanup

--- src/main/java/gg/test/Main.java
+++ src/main/java/gg/test/Main.java
@@ -1,18 +1,17 @@
 package gg.test;
 
 import com.merakianalytics.orianna.Orianna;
-import com.merakianalytics.orianna.types.common.Queue;
 import com.merakianalytics.orianna.types.common.Region;
-import com.merakianalytics.orianna.types.common.Season;
-import com.merakianalytics.orianna.types.core.championmastery.ChampionMasteries;
-import com.merakianalytics.orianna.types.core.championmastery.ChampionMastery;
 import com.merakianalytics.orianna.types.core.match.Match;
 import com.merakianalytics.orianna.types.core.match.MatchHistory;
-import com.merakianalytics.orianna.types.core.match.Participant;
-import com.merakianalytics.orianna.types.core.searchable.SearchableList;
 import com.merakianalytics.orianna.types.core.staticdata.Champion;
 import com.merakianalytics.orianna.types.core.staticdata.Champions;
 import com.merakianalytics.orianna.types.core.summoner.Summoner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.apache.log4j.BasicConfigurator;
 
 import java.util.ArrayList;
@@ -22,17 +21,36 @@ import java.util.Map;
 import java.util.Map.*;
 
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+//        String url = FXMLLoader.load(getClass().getClassLoader().getResource("../../../java/gg.test/View.fxml")).toString();
+//        System.out.println("DEBUG\n\n\n\n" + url + "\n\n\n\nDEBUG\n");
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("View.fxml"));
+        //        Parent root = FXMLLoader.load(getClass().getResource("../../../java/gg.test/View.fxml"));
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View.fxml"));
+//        ViewController controller = (ViewController) loader.getController();
+//        Parent root = (Parent)loader.load();
+        Scene scene = new Scene(root);
+//        stage.getIcons().add(new Image("/pics/nagyito2.png"));
+        stage.setTitle("Summoner Info");
+
+        stage.setWidth(993);
+        stage.setHeight(616);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 //    private static MatchHistory filterMatchHistory(final Summoner summoner) {
 //        final MatchHistory matchHistory = MatchHistory.forSummoner(summoner).withQueues(Queue.RANKED).withSeasons(Season.getLatest()).get();
 //        return matchHistory;
 //    }
 
-
     public static void main(final String[] args) {
         // Orianna framework git: https://github.com/meraki-analytics/orianna
         // RIOT API kulcs beállítása alternatív módszer: https://orianna.readthedocs.io/en/latest/configuring-orianna.html
+        launch(args);
         // Logolás bekapcsolása:
         BasicConfigurator.configure();
         String key = "RGAPI-760c5258-a109-419d-9a9d-5734e13b3189";
--- src/main/java/gg/test/ViewController.java
+++ src/main/java/gg/test/ViewController.java
@@ -1,4 +1,41 @@
 package gg.test;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
 public class ViewController {
+    @FXML
+    AnchorPane mainAnchorPane = new AnchorPane();
+    @FXML
+    AnchorPane mainAnchorPane2 = new AnchorPane();
+    @FXML
+    Button masteryButton = new Button();
+    @FXML
+    Pane  masteryPane = new Pane();
+    @FXML
+    Button topChampsButton = new Button();
+    @FXML
+    Pane  topChampsPane = new Pane();
+
+    @FXML
+    private void masteryButtonAction(ActionEvent event) throws Exception {
+        masteryPane.setVisible(false);
+        topChampsPane.setVisible(false);
+    }
+
+    @FXML
+    private void topChampsButtonAction(ActionEvent event) throws Exception {
+        masteryPane.setVisible(false);
+        topChampsPane.setVisible(false);
+    }
+
 }
