86865a90da4341a48105abd1b3d815a301a94933
Balazs Szanka <szankabalazs@gmail.com>
2021-05-08 17:07:14 +0200

    Removed the API key from the test class.

--- src/test/java/gg/test/ViewControllerTest.java
+++ src/test/java/gg/test/ViewControllerTest.java
@@ -32,7 +32,7 @@ class ViewControllerTest {
     void setUp() {
         BasicConfigurator.configure();
         // Enter your Riot API key before testing:
-        String key = "RGAPI-cf14e834-76ab-45f4-b671-e4c4fca10a52";
+        String key = "";
         Orianna.setRiotAPIKey(key);
     }
 
